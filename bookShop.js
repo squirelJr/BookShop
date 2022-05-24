eb6b37d00d825a3e45c1e06c6ebea23a836067b0
Davit Gozalishvili <84373811+squirelJr@users.noreply.github.com>
2022-05-24 19:06:30 +0400

    Working

--- bookShop.js
+++ bookShop.js
@@ -53,30 +53,49 @@ document.body.append(nav);
 
 let main = document.createElement("div");
 main.className="main";
-main.style.cssText=`display:flex; flex-direction:row;align-items:center;justify-content:space-around;width:100%; flex-wrap:wrap;height:80vw;border:2px solid black;`;
+main.style.cssText=`display:flex; flex-direction:row;align-items:center;justify-content:space-around;width:100%; flex-wrap:wrap;height:60vw;border:2px solid black;padding:30px;`;
 document.body.append(main);
 
-let books =[];
 function getBooks() {
     return fetch('Books.json')
       .then(resp=> {
         return resp.json();
       })
       .then(data=> {
-       
-        books.push(data);
+        for(let i=0;i<data.length;i++){
+          let card= document.createElement("div");
+          
+          card.style.cssText=` display:flex;flex-direction:column;width:20%;`;
+          let image = document.createElement("img");
+          let bookContent = document.createElement("div");
+          bookContent.className="bookContent"
+          bookContent.style.cssText=`width:200px;height:100px;display:flex;justify-content:space-around;align-items:space-around;flex-direction:column;`
+          image.src=data[i].imageLink;
+          card.className=`book${i+1}`
+          image.style.cssText=`width:150px;height:200px;`
+          console.log(image);
+         let title= document.createElement("h5")
+         title.className="bookTitle";
+         title.style.margin="5px 0px 0px 0px"
+         title.innerText=`Title:` +"  "+ `${data[i].title}`;
+         let author =document.createElement("p");
+         author.style.margin="5px 0pc 0px 0px"
+         author.className="bookAuthor";
+         let price = document.createElement("p");
+         price.className="price";
+         bookContent.style.padding="10px 0px 0px 0px"
+         price.innerText=`Price:${data[i].price}`;
+         author.innerText=`Author: ${data[i].author}`;
+         bookContent.append(author,title,price);
+          card.appendChild(image);
+          card.appendChild(bookContent);
+          main.appendChild(card);
+          card.addEventListener("mouseover",function(event){image.style.boxShadow ="rgb(150 150 150) 4px 3px 6px 6px"; });
+          card.addEventListener("mouseleave", function(event){image.style.boxShadow ="none"; })
+          
+      }
         
       })
   }
   getBooks();
-let arrayLength=books[0].length;
-for(let i=0;i<arrayLength;i++){
-    let card= document.createElement("div");
-    card.width="100px";
-    card.style.cssText=`boreder: 2px solid black`;
-    let image = document.createElement("img");
-    image.src=books[i].imageLink;
-    console.log(image);
-    card.appendChild(image);
-    main.appendChild(card);
-}
+ 
