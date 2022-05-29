772dfc4fdeac6d7c6c69e5376716572a060b7af9
Davit Gozalishvili <84373811+squirelJr@users.noreply.github.com>
2022-05-29 23:45:58 +0400

    UPD

--- bookShop.js
+++ bookShop.js
@@ -62,6 +62,24 @@ document.body.append(nav);
 let modal = document.createElement("div");
 modal.className = "modal";
 modal.style.cssText = `width:300px;border:1px solid black;max-height:84vh;position:absolute;top:110px;right:0px;background-color:#D7FFCF;z-index:5;display:flex;flex-direction:column;flex-wrap:nowrap; overflow-x:scroll;`
+let confirmbtn = document.createElement("button");
+confirmbtn.innerText = "Confirm Order";
+confirmbtn.style.cssText = `margin-bottom:20px;height:30px;font-weight:900;`;
+confirmbtn.addEventListener('mouseover', event => {
+  confirmbtn.style.cssText = `background-color:grey;color:white;margin-bottom:20px;height:30px;font-weight:900;cursor:pointer;`
+})
+confirmbtn.addEventListener('mouseleave', event => {
+  confirmbtn.style.cssText = `margin-bottom:20px;height:30px;font-weight:900;`;
+});
+
+let form = document.createElement("a");
+form.className = "formPage";
+form.href = "./form.html"
+confirmbtn.append(form);
+confirmbtn.addEventListener('click', event => {
+  window.location.href = "./form.html"
+});
+modal.prepend(confirmbtn)
 cartAnchor.addEventListener("mouseover", function (event) {
   this.style.cursor = "pointer";
 })
@@ -79,18 +97,32 @@ cartAnchor.addEventListener("click", function (event) {
 })
 
 
+let showModal = document.createElement("div");
+showModal.className = "showModal";
+showModal.style.cssText = `width:500px;border:1px solid black;height:300px;position:absolute;top:150px;right:30%;background-color:#D7FFCF;z-index:5;display:flex;flex-direction:column;flex-wrap:nowrap;align-items:center; overflow-x:scroll;`
+let closeModal = document.createElement("button");
+closeModal.innerText = "ClOSE";
+showModal.append(closeModal);
+closeModal.style.alignSelf = "flex-end";
+
+
+let removeBook = document.createElement("button");
+removeBook.innerText = "X";
+removeBook.style.alignSelf = "baseline";
+
 
 //Main 
 
 
-let grandTotal=0;
-let total=document.createElement("div");
-total.style.cssText=`font-weight:bolder;color: red;padding-bottom:5px;`
+let grandTotal = 0;
+let total = document.createElement("div");
+total.style.cssText = `font-weight:bolder;color: red;padding-bottom:5px;`
 let main = document.createElement("div");
 main.className = "main";
 main.style.cssText = `display:flex; flex-direction:row;align-items:center;justify-content:space-around;width:100%; flex-wrap:wrap;height:60vw;border:2px solid black;padding:30px;box-sizing:border-box;`;
 document.body.append(main);
 let booksInCart = [];
+let removable=[];
 
 function getBooks() {
   return fetch('Books.json')
@@ -135,26 +167,119 @@ function getBooks() {
         let addCart = document.createElement("button");
         addCart.className = "btn-buy";
         addCart.innerText = "ADD TO CART"
-        addCart.style.cssText = `width:150px;height:50px;background-color:white;color:green;border:1px solid green;box-shadow: 4px 5px 6px 6px #ccc;`
-
-        image.addEventListener("mouseover", function (event) {
+        addCart.style.cssText = `width:75px;height:50px;background-color:white;color:green;border:1px solid green;box-shadow: 4px 5px 6px 6px #ccc;`
+        let showMore = document.createElement("button");
+        showMore.className = "showMore";
+        showMore.innerText = "SHOW MORE"
+        showMore.style.cssText = `width:75px;height:50px;background-color:white;color:green;border:1px solid green;box-shadow: 4px 5px 6px 6px #ccc;`
+        let btnContainer = document.createElement("div");
+        btnContainer.cssText = `display:flex;flex-direction:row;justify-items:center;width:75px;height:55px;background-color:white;color:green;border:1px solid green;box-shadow: 4px 5px 6px 6px #ccc;`
+        btnContainer.append(addCart, showMore);
+        card.addEventListener("mouseover", function (event) {
           image.style.boxShadow = "rgb(150 150 150) 4px 3px 6px 6px";
           image.style.position = "relative";
           bookContent.innerHTML = "";
-          bookContent.append(addCart);
+          bookContent.append(btnContainer);
         });
-        image.addEventListener("mouseleave", function (event) {
+        card.addEventListener("mouseleave", function (event) {
           image.style.boxShadow = "none";
           bookContent.style.display = "flex";
-          bookContent.removeChild(addCart);
+          bookContent.removeChild(btnContainer);
           bookContent.append(author, title, price);
         });
+
+        addCart.addEventListener("mouseover", e => {
+          addCart.style.cursor = "pointer";
+          addCart.style.color = "white";
+          addCart.style.backgroundColor = "green";
+        });
+        addCart.addEventListener("mouseleave", e => {
+          addCart.style.cursor = "cursor";
+          addCart.style.color = "green";
+          addCart.style.backgroundColor = "white";
+        });
+        showMore.addEventListener("mouseover", e => {
+          showMore.style.cursor = "pointer";
+          showMore.style.color = "white";
+          showMore.style.backgroundColor = "green";
+        });
+        showMore.addEventListener("mouseleave", e => {
+          showMore.style.cursor = "cursor";
+          showMore.style.color = "green";
+          showMore.style.backgroundColor = "white";
+        });
+
+
+
+
+
+        addCart.addEventListener("click", e => {
+          added.innerText = "";
+          ++boughtAmount;
+
+
+          added.append(boughtAmount);
+          let a = addCart.parentElement;
+          let b = a.parentElement.parentElement;
+          // b.removeChild(removeBook);
+          bookContent.innerHTML = "";
+
+          bookContent.append(author, title, price);
+          b.prepend(removeBook);
+          let clone = b.cloneNode(true);
+
+          grandTotal += data[i].price;
+          
+          modal.append(clone);
+          b.removeChild(removeBook);
+          total.innerText = `TOTAL:${grandTotal}`;
+
+
+
+        });
+
+        let bool = false;
+        showMore.addEventListener("click", function (event) {
+
+          bool = !bool;
+          if (bool) {
+            let a = showMore.parentElement;
+            let b = a.parentElement.parentElement;
+            bookContent.innerHTML = "";
+            bookContent.style.height = "max-content";
+
+            let desc = data[i].description;
+            let p = document.createElement("p");
+            p.innerText = `descreption: ${desc}`;
+            bookContent.append(author, title, price, p);
+            let clone = b.cloneNode(true);
+            showModal.append(clone);
+            document.body.append(showModal);
+            main.style.opacity = "0.3";
+          } else {
+            document.body.removeChild(showModal);
+            main.style.opacity = "1";
+            showModal.removeChild(clone);
+          }
+
+        })
+        closeModal.addEventListener("click", e => {
+          document.body.removeChild(showModal);
+          main.style.opacity = "1";
+          showModal.innerHTML = "";
+          showModal.append(closeModal);
+        });
+
+
+
+        modal.prepend(total);
         let draggable;
         card.addEventListener("drag", ev => {
           let a = document.getElementById(`${ev.currentTarget.id}`);
-          bookContent.removeChild(addCart);
+          bookContent.removeChild(btnContainer);
           bookContent.append(author, title, price);
           a = a.cloneNode(true);
+          a.prepend(removeBook);
           console.log(a);
           draggable = a;
         });
@@ -163,68 +288,63 @@ function getBooks() {
         });
         modal.addEventListener("drop", ev => {
           ev.preventDefault();
-          
+
           if (draggable != undefined) {
             added.innerText = "";
             ++boughtAmount;
             console.log(data[i].price);
-            grandTotal+=data[i].price;
-           added.append(boughtAmount);
+            grandTotal += data[i].price;
+            added.append(boughtAmount);
             modal.append(draggable);
-            
+            total.innerText = `TOTAL:${grandTotal}`;
+            // modal.prepend(total);
+
           };
         });
+
+
+
+
 
         image.addEventListener('click', function (event) {
           added.innerText = "";
           ++boughtAmount;
-          if(image.onmouseover!=false){
-          bookContent.removeChild(addCart);
-          };
+
+          bookContent.removeChild(btnContainer);
+
           bookContent.append(author, title, price);
           added.append(boughtAmount);
           let classnames = image.parentElement.getAttribute("class");
           booksInCart.push(classnames);
           console.log(added.innerText);
-          grandTotal+=data[i].price;
+          grandTotal += data[i].price;
 
           booksInCart.forEach(x => {
             let a = document.getElementsByClassName(x).item(0);
+            a.prepend(removeBook);
             let clone = a.cloneNode(true);
             console.log(clone);
+
             modal.append(clone);
-            
-          
+            a.removeChild(removeBook);
+
+
           },
-         
-            modal.innerHTML = "",
+            modal.innerHTML="",
+
           );
-          total.innerText=`TOTAL:${grandTotal}`;
+          total.innerText = `TOTAL:${grandTotal}`;
           modal.prepend(total);
-          let confirm = document.createElement("button");
-          confirm.innerText = "Confirm Order";
-          confirm.style.cssText = `margin-bottom:20px;height:30px;font-weight:900;`;
-          confirm.addEventListener('mouseover', event => {
-            confirm.style.cssText = `background-color:grey;color:white;margin-bottom:20px;height:30px;font-weight:900;cursor:pointer;`
-          })
-          confirm.addEventListener('mouseleave', event => {
-            confirm.style.cssText = `margin-bottom:20px;height:30px;font-weight:900;`;
-          })
-          modal.prepend(confirm);
-          let form = document.createElement("a");
-          form.className = "formPage";
-          form.href = "./form.html"
-          confirm.append(form);
-          confirm.addEventListener('click', event => {
-            window.location.href = "./form.html"
-          })
-          console.log(booksInCart);
+          modal.prepend(confirmbtn);
+
+
+          console.log(removeBook.parentElement);
 
         })
 
-        
+
       }
-     
+
     })
 
 }
